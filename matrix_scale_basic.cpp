c47327256fc43b9e0f29d36af63a51a35cc2bd9a
Yarin Hazan <87584981+yarinh5@users.noreply.github.com>
2025-11-10 12:25:21 +0200

    Update matrix_scale_basic.cpp

--- matrix_scale_basic.cpp
+++ matrix_scale_basic.cpp
@@ -1,34 +1,44 @@
 #include <iostream>
 using namespace std;
 
-
-
 int main() {
-    // Write C++ code here
-    int a[2][2] = {{1,2},{3,4}};
-    int b[4][4] ={{0,0,0,0},{0,0,0,0}};
+    // הגדרת מטריצה בגודל 2x2 עם ערכים קבועים
+    int a[2][2] = {{1, 2}, {3, 4}};
+    
+    // הגדרת מטריצה חדשה בגודל 4x4 (מאותחלת לאפסים)
+    int b[4][4] = {{0, 0, 0, 0},
+                   {0, 0, 0, 0},
+                   {0, 0, 0, 0},
+                   {0, 0, 0, 0}};
 
+    // הדפסת המטריצה המקורית a
+    cout << "Original matrix (2x2):" << endl;
     for (int i = 0; i < 2; i++) {
-       for (int j = 0; j < 2; j++) {
+        for (int j = 0; j < 2; j++) {
             cout << a[i][j] << " ";
-       }
-       cout <<"\n";
+        }
+        cout << "\n";
     }
-    cout <<"\n";
-    
+
+    cout << "\n";
+
+    // מילוי המטריצה החדשה b על בסיס a
+    // כל איבר ב-b נקבע לפי a[i/2][j/2]
+    // כלומר, כל איבר במטריצה a מוכפל לגוש בגודל 2x2 במטריצה b
     for (int i = 0; i < 4; i++) {
-       for (int j = 0; j < 4; j++) {
+        for (int j = 0; j < 4; j++) {
             b[i][j] = a[i/2][j/2];
-       }
+        }
     }
-    
+
+    // הדפסת המטריצה החדשה b לאחר ההכפלה
+    cout << "Scaled matrix (4x4):" << endl;
     for (int i = 0; i < 4; i++) {
-       for (int j = 0; j < 4; j++) {
+        for (int j = 0; j < 4; j++) {
             cout << b[i][j] << " ";
-       }
-       cout <<"\n";
+        }
+        cout << "\n";
     }
-    
 
     return 0;
 }
