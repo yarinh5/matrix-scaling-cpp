6c8451af4de263493676e61beba1c041aa7f28df
Yarin Hazan <87584981+yarinh5@users.noreply.github.com>
2025-11-10 12:47:29 +0200

    Update matrix_scale_dynamic.cpp

--- matrix_scale_dynamic.cpp
+++ matrix_scale_dynamic.cpp
@@ -2,66 +2,66 @@
 using namespace std;
 
 int main() {
-    int N, scale;
+    int n, s;
 
-    cout << "Enter matrix size (N): ";
-    cin >> N;
+    cout << "Enter matrix size : ";
+    cin >> n;
 
-    cout << "Enter scale factor: ";
-    cin >> scale;
+    cout << "Enter scale factor : ";
+    cin >> s;
 
     // הקצאה דינמית של מטריצה מקורית
-    int** a = new int*[N];
-    for (int i = 0; i < N; i++)
-        a[i] = new int[N];
+    int** a = new int*[n];
+    for (int i = 0; i < n; i++)
+        a[i] = new int[n];
 
     // הזנת ערכים למטריצה
-    cout << "\nEnter " << N*N << " elements:\n";
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+    cout << "\nEnter " << n * n << " elements:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             cin >> a[i][j];
         }
     }
 
     // חישוב גודל המטריצה החדשה
-    int newSize = N * scale;
+    int newMat = n * s;
 
     // הקצאה דינמית של מטריצה מוגדלת
-    int** b = new int*[newSize];
-    for (int i = 0; i < newSize; i++)
-        b[i] = new int[newSize];
+    int** b = new int*[newMat];
+    for (int i = 0; i < newMat; i++)
+        b[i] = new int[newMat];
 
     // מילוי המטריצה החדשה
-    for (int i = 0; i < newSize; i++) {
-        for (int j = 0; j < newSize; j++) {
-            b[i][j] = a[i / scale][j / scale];
+    for (int i = 0; i < newMat; i++) {
+        for (int j = 0; j < newMat; j++) {
+            b[i][j] = a[i / s][j / s];
         }
     }
 
     // הדפסה של המטריצה המקורית
-    cout << "\nOriginal matrix (" << N << "x" << N << "):\n";
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+    cout << "\nOriginal matrix (" << n << "x" << n << "):\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             cout << a[i][j] << " ";
         }
         cout << "\n";
     }
 
     // הדפסה של המטריצה החדשה
-    cout << "\nScaled matrix (" << newSize << "x" << newSize << "):\n";
-    for (int i = 0; i < newSize; i++) {
-        for (int j = 0; j < newSize; j++) {
+    cout << "\nScaled matrix (" << newMat << "x" << newMat << "):\n";
+    for (int i = 0; i < newMat; i++) {
+        for (int j = 0; j < newMat; j++) {
             cout << b[i][j] << " ";
         }
         cout << "\n";
     }
 
     // שחרור זיכרון
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < n; i++)
         delete[] a[i];
     delete[] a;
 
-    for (int i = 0; i < newSize; i++)
+    for (int i = 0; i < newMat; i++)
         delete[] b[i];
     delete[] b;
 
